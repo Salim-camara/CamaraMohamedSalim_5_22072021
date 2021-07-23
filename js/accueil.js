7f3b69618d4ac142c94895f9c2c962447cb6afac
Camara Mohamed Salim <m.salimcamara@gmail.com>
2021-07-23 21:02:30 +0200

    Ajout des cards en html

--- js/accueil.js
+++ js/accueil.js
@@ -10,3 +10,31 @@ window.addEventListener('scroll', () => {
         navigation.classList.remove('top_bar_bg');
     }
 });
+
+
+// ******************************API cards
+
+let url = 'http://localhost:3000/api/cameras';
+let cards = document.querySelector('.container');
+
+fetch(url).then((response) =>
+    response.json().then((data) => {
+        console.log(data);
+
+        cards.innerHTML = `<div class="card">
+        <span class="card--bg"></span>
+        <div class="card__second">
+            <h3 class="card__second--h3">${data[0].name}</h3>
+            <span class="card__second--desc">Description: Lorem ipsum dolor sit amet, consecteLorem ipsum dolor sit amet, consecteLorem ipsum dolor sit amet, consecte</span>
+            <span class="card__second--prix">prix: 2049 euros</span>
+        </div>
+        <button class="card--button">Ajouter au panier</button>
+    </div>`
+    }));
+
+
+
+
+
+
+
