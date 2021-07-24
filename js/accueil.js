abfc9c77915ade7211b7692a0d3ac32b302c42bb
Camara Mohamed Salim <m.salimcamara@gmail.com>
2021-07-24 13:12:43 +0200

    Ajout des divs en javascript

--- js/accueil.js
+++ js/accueil.js
@@ -1,13 +1,13 @@
 // *************************Animation navbar**********************
 
-const navigation = document.querySelector('.top_bar');
+const NAVIGATION = document.querySelector('.top_bar');
 
 window.addEventListener('scroll', () => {
 
     if(window.scrollY > 600) {
-        navigation.classList.add('top_bar_bg');
+        NAVIGATION.classList.add('top_bar_bg');
     }else {
-        navigation.classList.remove('top_bar_bg');
+        NAVIGATION.classList.remove('top_bar_bg');
     }
 });
 
@@ -18,32 +18,35 @@ window.addEventListener('scroll', () => {
 
 let url = 'http://localhost:3000/api/cameras';
 let cards = document.querySelector('.container');
+
 // Appel de l'API
 fetch(url).then((response) =>
     response.json().then((data) => {
         console.log(data.length);
-        for (let i = 0; i < 5; i++) {
+        for (let i = 0; i < data.length; i++) {
 
+            // Ajout du point dans les prix
+            console.log(data[i].price);
+            let prix = data[i].price;
+            console.log(prix.number.length);
+
+            // Création de l'HTML
             let card = document.createElement('div');
             card.classList.add('card');
             cards.appendChild(card);
-            card.innerHTML = '<strong>bonjour</strong>';
+            card.innerHTML = `<span class="card--bg cardbg--${i}"></span>
+            <div class="card__second">
+                <h3 class="card__second--h3">${data[i].name}</h3>
+                <span class="card__second--desc">Description: ${data[i].description}</span>
+                <span class="card__second--prix">prix: ${data[i].price} euros</span>
+            </div>
+            <button class="card--button">Ajouter au panier</button>`;
 
 
-
-
-
-
-        let saucisse = document.createElement('div');
-        saucisse.id = "salut";
-        
-        let node = document.createTextNode('');
-
-        saucisse.appendChild(node);
-
-        cards.appendChild(saucisse);
-
-        saucisse.innerHTML = "bonjour je suis moche";
+            // Background-image
+            let spanBg = document.querySelector(`.cardbg--${i}`);
+            spanBg.style.backgroundImage = `url('${data[i].imageUrl}')`;
+            
 
 
     }
