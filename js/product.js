2c40384c6ecbd2f9c7b36fd8155ffeed6d14439c
Camara Mohamed Salim <m.salimcamara@gmail.com>
2021-07-25 23:38:02 +0200

    Ajout du message d'erreur pour les lentilles

--- js/product.js
+++ js/product.js
@@ -2,9 +2,7 @@
 
 // Récupération de l'ID de l'URL
 let urlBrut = document.location.href;
-console.log(urlBrut);
 let id = urlBrut.slice(35);
-console.log(id);
 
 // URL à aller chercher
 let url = `http://localhost:3000/api/cameras/${id}`;
@@ -13,7 +11,6 @@ let main = document.querySelector('.main');
 // Appel de l'API
 fetch(url).then((response) =>
     response.json().then((data) => {
-        console.log(data);
 
         // Suppression des 0 à la fin des prix
         let prix = `${data.price}`.slice(0, -2);
@@ -27,9 +24,11 @@ fetch(url).then((response) =>
             <h3 class="card__second--h3">${data.name}</h3>
             <span class="card__second--desc"><strong>Description</strong> : ${data.description}</span>
             <span class="card__second--prix"><strong>Prix</strong> : ${prix}<strong>€</strong></span>
-            <select class="card__second--select">
-                <option selected>Choisir un objectif<option>
-            </select>
+            <form>
+                <select class="card__second--select">
+                    <option value="default">Choisir un objectif<option>
+                </select>
+            </form>
         </div>
         <button class="card--button">Ajouter au panier</button>`;
 
@@ -40,6 +39,7 @@ fetch(url).then((response) =>
             let select = document.querySelector('.card__second--select');
             let option = document.createElement('option');
             option.classList.add('option');
+            option.value = `${i}`;
             select.appendChild(option);
 
             // Construction de l'option
@@ -47,7 +47,37 @@ fetch(url).then((response) =>
             select.appendChild(option);
         }
 
+
+        // Obtention de l'index de la lentille
+        let select = document.querySelector('.card__second--select');
+        let index = null;
+        let err = document.createElement('p');
+        err.classList.add('erreur');
+        select.addEventListener('change', () => {
+            err.remove();
+            index = `${select.selectedIndex} ` - 2;
+
+            // condition
+            if (index == -2 || index == -1) {
+                let endCard = document.querySelector('.card__second');
+                err.innerHTML = 'Veuillez choisir un objectif';
+                endCard.appendChild(err);
+
+            } else {
+                console.log(index);
+            };
+        });
+
+
         // Background-image
         let spanBg = document.querySelector(`.card--bg`);
         spanBg.style.backgroundImage = `url('${data.imageUrl}')`;
     }));
+
+
+
+
+
+// ***********************************Confirmation de la selection*******************************
+
+
